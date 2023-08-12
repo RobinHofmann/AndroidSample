c9076e3bcb7173fe8f03d41fbfdb2dd6a35c35d8
RobinHofmann <hofmannrobin95@gmail.com>
2023-08-12 13:29:21 +0200

    implemented simple currency converter

--- app/src/main/java/com/example/androidsample/MainActivity.java
+++ app/src/main/java/com/example/androidsample/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.androidsample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void convertCurrency(View view) {
+        EditText dollarText = findViewById(R.id.dollarText);
+        TextView textView = findViewById(R.id.textView);
+
+        if (!dollarText.getText().toString().equals("")){
+            float dollarValue = Float.parseFloat(dollarText.getText().toString());
+            float euroValue = dollarValue * 0.85F;
+            textView.setText(String.format(Locale.ENGLISH,"%.2f", euroValue));
+        } else {
+            textView.setText(R.string.no_value_string);
+        }
+
+
     }
 }
