86a38f76822472be7816fad9fcd6e21f1d34e6b7
RobinHofmann <hofmannrobin95@gmail.com>
2023-08-19 19:26:51 +0200

    created simple dropdown that allows a user to type in text with autocomplete. NullPointerExceptions need to be targeted

--- app/src/main/java/com/example/androidsample/MainActivity.java
+++ app/src/main/java/com/example/androidsample/MainActivity.java
@@ -5,9 +5,11 @@ import androidx.recyclerview.widget.ListAdapter;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -30,6 +32,10 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class MainActivity extends AppCompatActivity {
 
     private TextView textViewResult;
+
+    private AutoCompleteTextView actvLeft;
+    private AutoCompleteTextView actvRight;
+
     private Button button;
     private ArrayList<String> exchangeRates;
     Map<String, Double> conversionRates;
@@ -45,8 +51,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         TextView textViewResult = findViewById(R.id.textViewResult);
         Button button = (Button) findViewById(R.id.button);
+
+
+        // BUTTON ---------------
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v){
                 //Log.d("BUTTONS", "User tapped the Button");
@@ -54,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // GET Request -----------
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://v6.exchangerate-api.com/v6/dcf307f391433e5e3fd1aab9/latest/")
                 .addConverterFactory(GsonConverterFactory.create())
@@ -76,25 +87,39 @@ public class MainActivity extends AppCompatActivity {
 
                 exchangeRates = new ArrayList<String>(conversionRates.keySet());
 
-                Spinner spinnerLeft = findViewById(R.id.spinner_currency_left);
-                Spinner spinnerRight = findViewById(R.id.spinner_currency_right);
+                ArrayAdapter<String> adapterLeft = new ArrayAdapter<String>(MainActivity.this,
+                        android.R.layout.select_dialog_item, exchangeRates);
 
-                ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this,
-                        android.R.layout.simple_spinner_dropdown_item, exchangeRates);
-                spinnerLeft.setAdapter(adapter);
-                spinnerRight.setAdapter(adapter);
-                spinnerLeft.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                ArrayAdapter<String> adapterRight = new ArrayAdapter<String>(MainActivity.this,
+                        android.R.layout.select_dialog_item, exchangeRates);
+
+                actvLeft = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextViewLeft);
+                actvRight = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextViewRight);
+
+
+                actvLeft.setOnFocusChangeListener(new View.OnFocusChangeListener(){
                     @Override
-                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                        String itemValue = parent.getItemAtPosition(position).toString();
-                        Toast.makeText(parent.getContext(), itemValue, Toast.LENGTH_SHORT).show();
-                    }
-
-                    @Override
-                    public void onNothingSelected(AdapterView<?> parent) {
-
+                    public void onFocusChange(View v, boolean hasFocus) {
+                        if (hasFocus) {
+                            actvLeft.showDropDown();
+                        }
                     }
                 });
+                actvRight.setOnFocusChangeListener(new View.OnFocusChangeListener(){
+                    @Override
+                    public void onFocusChange(View v, boolean hasFocus) {
+                        if (hasFocus) {
+                            actvRight.showDropDown();
+                        }
+                    }
+                });
+
+                actvLeft.setThreshold(0);//will start working from first character
+                actvLeft.setAdapter(adapterLeft);//setting the adapter data into the AutoCompleteTextView
+
+                actvRight.setThreshold(0);//will start working from first character
+                actvRight.setAdapter(adapterRight);//setting the adapter data into the AutoCompleteTextView
+
             }
 
             @Override
@@ -110,20 +135,18 @@ public class MainActivity extends AppCompatActivity {
         TextView textViewResult = findViewById(R.id.textViewResult);
 
         if (!inputText.getText().toString().equals("")){
-            Spinner spinnerLeft = findViewById(R.id.spinner_currency_left);
-            Spinner spinnerRight = findViewById(R.id.spinner_currency_right);
 
             float baseValue = Float.parseFloat(inputText.getText().toString()); //value to convert
 
-            String baseCurrency = spinnerLeft.getSelectedItem().toString();
-            String targetCurrency = spinnerRight.getSelectedItem().toString();
+            String baseCurrency = actvLeft.getText().toString();
+            String targetCurrency = actvRight.getText().toString();
 
             float baseExchangeRate = conversionRates.get(baseCurrency).floatValue();
             float targetExchangeRate = conversionRates.get(targetCurrency).floatValue();
 
             float targetValue = (baseValue/baseExchangeRate)*targetExchangeRate;
 
-            textViewResult.setText(String.format(Locale.ENGLISH,"%.2f", targetValue));
+            textViewResult.setText(String.format(Locale.ENGLISH,"%.2f %s", targetValue, targetCurrency));
         } else {
             textViewResult.setText(R.string.no_value_string);
         }
